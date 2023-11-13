343c16230efddf64f348613f523949e8d61084b2
jay-ci <jeancharlesvalentin13@gmail.com>
2023-11-13 09:50:50 +0100

    création d'animation

--- src/Layouts/header.jsx
+++ src/Layouts/header.jsx
@@ -10,15 +10,17 @@ function Header(){
                 Votre navigateur ne prend pas en compte le format de la video
         </video>
         </div>
-        <div className='presentation'>
-            <img src="https://i.goopics.net/q1o7xr.jpg" alt="Developpeur" className="developpeur"/>
-            <h1>Jean-Charles VALENTIN</h1>
-            <p className="description">Développeur et Intégrateur Web</p>
-        </div>
-        <div id="aboutMe-container">
-            <div className="aboutMe-description">
-                <p className="introduction">Developpeur passioné par le domaine du Web.Je conçois et réalise vos sites du cahier des charges à la mise en ligne.</p>
-                <p className="presentation">Je maitrise de nombreux langage qui me permettront de rendre votre site fiable et fluide afin d'offrir la meilleure experience possible à vos utilisateurs</p>
+        <div className='presentation-container'>
+            <div className='presentation'>
+                <img src="https://i.goopics.net/q1o7xr.jpg" alt="Developpeur" className="developpeur"/>
+                <h1 id='Portfolio-title'>Jean-Charles VALENTIN</h1>
+                <p className="description">Développeur et Intégrateur Web</p>
+            </div>
+            <div id="aboutMe-container">
+                <div className="aboutMe-description">
+                    <p className="introduction">Developpeur passioné par le domaine du Web.Je conçois et réalise vos sites du cahier des charges à la mise en ligne.</p>
+                    <p className="presentation">Je maitrise de nombreux langage qui me permettront de rendre votre site fiable et fluide afin d'offrir la meilleure experience possible à vos utilisateurs</p>
+                </div>
             </div>
         </div>
     </div>

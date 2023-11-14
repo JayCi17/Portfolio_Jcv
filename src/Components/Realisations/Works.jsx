528556c18d717c2a69011f94cab0d9967916bf4b
jay-ci <jeancharlesvalentin13@gmail.com>
2023-11-14 13:18:42 +0100

    mise en place de la partie realisations

--- src/Components/Realisations/Works.jsx
+++ src/Components/Realisations/Works.jsx
@@ -12,6 +12,129 @@ function Works(){
                             <a href="" className="Cliquez">Cliquez ici</a>
                         </div>
                     </div>
+                    <div className="competences-problemes">
+                        <div className="competences">
+                            <h3>Compètences acquises</h3>
+                            <ul>
+                                <li>React</li>
+                                <li>Html/Css</li>
+                                <li>Javascript</li>
+                            </ul>
+                        </div>
+                        <div className="problemes">
+                            <h3>Problèmatique résolue</h3>
+                            <ul>
+                                <li>Importation incorrecte de composants</li>
+                                <li>Erreur de syntaxe</li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+                <article className="work-article">
+                    <img src="https://i.goopics.net/nhox4y.png" alt="visuel du projet"/>
+                    <div className="overlay">
+                        <div className="Cliquez-container">
+                            <a href="https://jayci17.github.io/NinaCarducci/" className="Cliquez">Cliquez ici</a>
+                        </div>
+                    </div>
+                    <div className="competences-problemes">
+                        <div className="competences">
+                            <h3>Compètences acquises</h3>
+                            <ul>
+                                <li>Seo</li>
+                                <li>Accessibilité</li>
+                                <li>Javascript</li>
+                                <li>Html/Css</li>
+                            </ul>
+                        </div>
+                        <div className="problemes">
+                            <h3>Problèmatique résolue</h3>
+                            <ul>
+                                <li>Amélioration de la performance</li>
+                                <li>Optimisation du referencement</li>
+                                <li>Optimisation de l'accesabilité</li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+                <article className="work-article">
+                    <img src="https://i.goopics.net/ckghmr.png" alt="visuel du projet"/>
+                    <div className="overlay">
+                        <div className="Cliquez-container">
+                            <a href="https://jayci17.github.io/Projet_4/" className="Cliquez">Cliquez ici</a>
+                        </div>
+                    </div>
+                    <div className="competences-problemes">
+                        <div className="competences">
+                            <h3>Compètences acquises</h3>
+                            <ul>
+                                <li>Responsive</li>
+                                <li>Animations Css</li>
+                                <li>Html/Css</li>
+                            </ul>
+                        </div>
+                        <div className="problemes">
+                            <h3>Problèmatique résolue</h3>
+                            <ul>
+                                <li>Developper en mobile-first</li>
+                                <li>Respect de la charte graphique</li>
+                                <li>Création d'un loader</li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+                <article className="work-article">
+                    <img src="https://i.goopics.net/5htuqh.png" alt="visuel du projet"/>
+                    <div className="overlay">
+                        <div className="Cliquez-container">
+                            <a href="" className="Cliquez">Cliquez ici</a>
+                        </div>
+                    </div>
+                    <div className="competences-problemes">
+                        <div className="competences">
+                            <h3>Compètences acquises</h3>
+                            <ul>
+                                <li>Responsive</li>
+                                <li>React</li>
+                                <li>Redux</li>
+                                <li>MongoDb</li>
+                                <li>Api</li>
+                                <li>Green IT</li>
+                            </ul>
+                        </div>
+                        <div className="problemes">
+                            <h3>Problèmatique résolue</h3>
+                            <ul>
+                                <li>Gérer une base de données</li>
+                                <li>Etablissement des routes</li>
+                            </ul>
+                        </div>
+                    </div>
+                </article>
+                <article className="work-article">
+                    <img src="https://i.goopics.net/t9gtxl.png" alt="visuel du projet"/>
+                    <div className="overlay">
+                        <div className="Cliquez-container">
+                            <a href="" className="Cliquez">Cliquez ici</a>
+                        </div>
+                    </div>
+                    <div className="competences-problemes">
+                        <div className="competences">
+                            <h3>Compètences acquises</h3>
+                            <ul>
+                                <li>Responsive</li>
+                                <li>React</li>
+                                <li>Debugage d'un site</li>
+                                <li>React Dev Tools</li>
+                            </ul>
+                        </div>
+                        <div className="problemes">
+                            <h3>Problèmatique résolue</h3>
+                            <ul>
+                                <li>Trouver les anomalies</li>
+                            </ul>
+                        </div>
+                    </div>
                 </article>
             </div>
         </section>
